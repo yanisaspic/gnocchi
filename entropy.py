5f9ba5214e00217376947964aa799a0813812717
Yanis Asloudj <yanis.sloudj@gmail.com>
2022-10-11 17:34:38 +0200

    data framed

--- entropy.py
+++ entropy.py
@@ -11,14 +11,28 @@ from goatools.gosubdag.gosubdag import GoSubDag
 import pandas as pd
 
 from skbio.diversity.alpha import shannon
-from math import exp
+from math import exp, log
 import matplotlib.pyplot as plt
 
 
-def get_term_leaves(gosubdag, go_id):
-    return
+### V A R S
+ontologies = ['go-basic.obo', 'hp.obo']
+onto_roots = {'BP': 'GO:0008150', 'MF': 'GO:0003674', 'CC': 'GO:0005575', 'human_phenotype': 'HP:0000001'}
 
-def get_term_data(gosubdag, go_id):
+
+def get_term_leaves(godag):
+    """Return a dict associating an ontology term id (key) to its leaves (value)."""
+    onto_leaves = set()
+    for term in godag:
+        children = godag[term].get_all_children()
+        if len(children) == 0:
+            onto_leaves.add(term)
+    term_leaves = {}
+    for term in godag:
+        term_leaves[term] = onto_leaves.intersection(godag[term].get_all_children())
+    return term_leaves
+
+def get_term_data(gosubdag, go_id, term_leaves):
     """Return a dict associating an ontology term id (key) to its data (value).
     Duplicate GODag terms (alternate ids) are ignored.
 
@@ -29,48 +43,40 @@ def get_term_data(gosubdag, go_id):
     term_data = {term.id: {
         'name': term.GO_name,
         'namespace': term.NS,
+        'descendants': term.dcnt + 1,  # descendants: nodes below term. (+1) to compute Shannon entropy
         'level': term.level,            # level: shortest path to term node from root
         'depth': term.depth,            # depth: longest path to term node from root
-        'descendants': term.dcnt + 1}}  # descendants: nodes below term. (+1) to compute Shannon entropy
+        'leaves': len(term_leaves[go_id])}}
 
-    # subsumers: nodes term, term included (+1).
-    try:
-        term_data[term.id]['subsumers'] = len(gosubdag.rcntobj.go2ancestors[term.id])
+    try:    # subsumers: nodes term, term included (+1).
+        term_data[term.id]['subsumers'] = len(gosubdag.rcntobj.go2ancestors[term.id]) + 1
     except KeyError:
-        term_data[term.id]['subsumers'] = 0
-    term_data[term.id]['subsumers'] += 1
-
+        term_data[term.id]['subsumers'] = 1
     return term_data
 
-def get_onto_df(ontology):
-    """Return a dataframe associating the terms of an ontology (rows) to their respective data (columns)."""
+def get_onto_df(obo):
+    """Return a dataframe associating the terms of a .obo ontology (rows) to their respective data (columns)."""
     onto_dict = {}
-    godag = get_godag(ontology)
-    go_ids_selected = (list(godag.keys()))
-    gosubdag = GoSubDag(go_ids_selected, godag)
-    for go_id in go_ids_selected:
-        onto_dict.update(get_term_data(gosubdag, go_id))
-    return pd.DataFrame.from_dict(onto_dict, orient='index')
+    godag = get_godag(obo)
+    gosubdag = GoSubDag(list(godag.keys()), godag)
+    term_leaves = get_term_leaves(godag)
+    for go_id in list(godag.keys()):
+        onto_dict.update(get_term_data(gosubdag, go_id, term_leaves))
+    return pd.DataFrame.from_dict(onto_dict, orient='index').sort_values(by=['namespace', 'descendants'], ascending=False)
 
-onto_df = get_onto_df('go-basic.obo')
-print(onto_df)
-
-# check if removing the most frequent terms increase Shannon entropy / check terms
-l_entropy = []
-l_terms = []
-n_terms_total = len(test_corpus)
-print(n_terms_total)
-best_entropy = exp(shannon(test_corpus))
-curr_entropy = best_entropy
-l_entropy.append(curr_entropy)
-l_terms.append(1-len(test_corpus)/n_terms_total)
-while len(test_corpus)>1:
-    test_corpus.pop(0)
-    curr_entropy = exp(shannon(test_corpus))
-    l_entropy.append(curr_entropy)
-    l_terms.append(1-len(test_corpus)/n_terms_total)
+def get_ic_sanchez(onto_df):
+    """Return a pandas Series corresponding to the Information Content (cf. Sanchez et al. 2011) of the terms in the ontology dataframe."""
+    root_terms = onto_df.namespace.apply(lambda x: onto_roots[x])
+    max_leaves = root_terms.apply(lambda x: onto_df.loc[x].leaves)
+    a = (onto_df.leaves / onto_df.subsumers + 1) / (max_leaves + 1)
+    return a.apply(lambda x: -log(x))
 
 
-# display the behavior of the Shannon entropy 
-plt.plot(l_terms, l_entropy)
-plt.show()
+### M A I N
+onto_df = pd.DataFrame()
+for obo in ontologies:
+    onto_df = pd.concat([onto_df, get_onto_df(obo)])
+onto_df['sanchez'] = get_ic_sanchez(onto_df)
+with pd.ExcelWriter('ontology_data.xlsx') as writer:
+    for source in onto_df.namespace.unique():
+        onto_df[onto_df.namespace == source].to_excel(writer, sheet_name=source)
