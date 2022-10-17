04ad12720331e6ca193a9251fe7c865d21ac11d9
Yanis Asloudj <yanis.sloudj@gmail.com>
2022-10-17 17:03:18 +0200

    need to think

--- information.py
+++ information.py
@@ -8,6 +8,10 @@ Quantify the information content of a corpus of ontology terms.
 
 from math import exp
 from skbio.diversity.alpha import shannon
+from goatools.base import get_godag
+from goatools.gosubdag.gosubdag import GoSubDag
+from goatools.godag_plot import plot_gos
+from sys import stdout
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -19,9 +23,25 @@ def get_corpus_statistics(onto_df):
         'shannon': shannon(onto_df.descendants),
         # 'mean_level': onto_df.level.mean(),
         # 'mean_level/depth': onto_df['level/depth'].mean(),
-        'mean_sanchez': onto_df.sanchez.mean()
+        'sanchez': onto_df.sanchez.mean()
         # 'std_sanchez': onto_df.sanchez.std()
     }
+
+def save_ontology(filename, godag):
+    """save an ontology file."""
+    ontology = open(filename, 'wt')
+    ontology.write('format-version: 1.2\n\n')
+    for term in godag:
+        ontology.write('[Term]\n')
+        ontology.write(f'id: {term}\n')
+        ontology.write(f'name: {godag[term].name}\n')
+        for parent in godag[term]._parents:
+            try:
+                ontology.write(f'is_a: {parent} ! {godag[parent].name}\n')
+            except KeyError:
+                pass
+        ontology.write('\n')
+    
 
 def get_corpus_evolution(onto_df):
     """Returns a dict associating a decreasing corpus size to its information content."""
@@ -30,30 +50,43 @@ def get_corpus_evolution(onto_df):
         corpus_evolution[onto_df.shape[0]] = get_corpus_statistics(onto_df)
         onto_df = onto_df.iloc[1:]
     corpus_df = pd.DataFrame.from_dict(corpus_evolution, orient='index').applymap(lambda x: exp(x))
-    return (corpus_df - corpus_df.min()) / (corpus_df.max() - corpus_df.min())
+    corpus_df = (corpus_df - corpus_df.min()) / (corpus_df.max() - corpus_df.min())
+    corpus_df['dist'] = abs(corpus_df['shannon'] - corpus_df['sanchez'])
+    corpus_df['delta'] = corpus_df['delta']
+    return corpus_df
 
-
-### M A I N
-fig, axs = plt.subplots(2, 2)
+# ### M A I N
 onto_df = pd.read_csv('ontology_direct_data.csv', index_col=0)
-### BP
 onto = onto_df[onto_df.namespace=='BP']
 corpus_evolution = get_corpus_evolution(onto)
-corpus_evolution.index = corpus_evolution.index.map(lambda x: 1 - x/corpus_evolution.index[0])
-corpus_evolution.plot(y=['shannon', 'mean_sanchez'], use_index=True, ax=axs[0, 0], title='BP')
-### CC
-onto = onto_df[onto_df.namespace=='CC']
-corpus_evolution = get_corpus_evolution(onto)
-corpus_evolution.index = corpus_evolution.index.map(lambda x: 1 - x/corpus_evolution.index[0])
-corpus_evolution.plot(y=['shannon', 'mean_sanchez'], use_index=True, ax=axs[0, 1], legend=None, title='CC')
-### MF
-onto = onto_df[onto_df.namespace=='reactome']
-corpus_evolution = get_corpus_evolution(onto)
-corpus_evolution.index = corpus_evolution.index.map(lambda x: 1 - x/corpus_evolution.index[0])
-corpus_evolution.plot(y=['shannon', 'mean_sanchez'], use_index=True, ax=axs[1, 0], legend=None, title='reactome')
-### human_phenotype
-onto = onto_df[onto_df.namespace=='human_phenotype']
-corpus_evolution = get_corpus_evolution(onto)
-corpus_evolution.index = corpus_evolution.index.map(lambda x: 1 - x/corpus_evolution.index[0])
-corpus_evolution.plot(y=['shannon', 'mean_sanchez'], use_index=True, ax=axs[1, 1], legend=None, title='HPO')
-plt.show()
+n_terms = corpus_evolution[corpus_evolution.dist == corpus_evolution.dist.min()].index[0]
+removed_terms = set(onto.iloc[:-n_terms].index)
+godag = get_godag('go-basic.obo')
+
+deep_terms = set(removed_terms).intersection(set(godag.keys()))
+for term in deep_terms:
+    x=godag.pop(term)
+save_ontology('saved.obo', godag)
+
+# fig, axs = plt.subplots(2, 2)
+# ### BP
+# onto = onto_df[onto_df.namespace=='BP']
+# corpus_evolution = get_corpus_evolution(onto)
+# corpus_evolution.index = corpus_evolution.index.map(lambda x: 1 - x/corpus_evolution.index[0])
+# corpus_evolution.plot(y=['shannon', 'mean_sanchez'], use_index=True, ax=axs[0, 0], title='BP')
+# ### CC
+# onto = onto_df[onto_df.namespace=='CC']
+# corpus_evolution = get_corpus_evolution(onto)
+# corpus_evolution.index = corpus_evolution.index.map(lambda x: 1 - x/corpus_evolution.index[0])
+# corpus_evolution.plot(y=['shannon', 'mean_sanchez'], use_index=True, ax=axs[0, 1], legend=None, title='CC')
+# ### MF
+# onto = onto_df[onto_df.namespace=='reactome']
+# corpus_evolution = get_corpus_evolution(onto)
+# corpus_evolution.index = corpus_evolution.index.map(lambda x: 1 - x/corpus_evolution.index[0])
+# corpus_evolution.plot(y=['shannon', 'mean_sanchez'], use_index=True, ax=axs[1, 0], legend=None, title='reactome')
+# ### human_phenotype
+# onto = onto_df[onto_df.namespace=='human_phenotype']
+# corpus_evolution = get_corpus_evolution(onto)
+# corpus_evolution.index = corpus_evolution.index.map(lambda x: 1 - x/corpus_evolution.index[0])
+# corpus_evolution.plot(y=['shannon', 'mean_sanchez'], use_index=True, ax=axs[1, 1], legend=None, title='HPO')
+# plt.show()
